ed934d6a88692d0579927858636d12cee7b051f9
Ilya Rakhlin <i.rakhlin@gmail.com>
2021-09-10 01:13:28 -0400

    add config file

---
+++ rtlamr2mqtt/data/run.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bashio
+set -e
+
+CONFIG="/etc/rtlamr2mqtt.yaml"
+
+bashio::log.info "Creating rtlamr2mqtt configuration..."
+
+# Create main config
+MQTTHOST=$(bashio::config 'mqtt_host')
+MQTTUSER=$(bashio::config 'mqtt_user')
+MQTTPASSWORD=$(bashio::config 'mqtt_password')
+METERID=$(bashio::config 'meter_id')
+
+{
+    echo "general:"
+    echo "  sleep_for: 0"
+    echo "mqtt:"
+    echo "  host: ${MQTTHOST}"
+    echo "  user: ${MQTTUSER}"
+    echo "  password: ${MQTTPASSWORD}"
+    echo "  ha_autodiscovery: true"
+    echo "  ha_autodiscovery_topic: homeassistant"
+    echo "custom_parameters:"
+    echo "  rtltcp: \"-s 2048000\""
+    echo "  rtlamr: \"\""
+    echo "meters:"
+    echo "  - id: ${METERID}"
+    echo "    protocol: idm"
+    echo "    name: main_power"
+    echo "    format: \"######.##\""
+    echo "    unit_of_measurement: kWh"
+    echo "    icon: mdi:transmission-tower"
+} > "${CONFIG}"
